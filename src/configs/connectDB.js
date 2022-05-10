97b631a38c02bc8a328e9d8e8a899f740729e471
hoanghai1211 <hoanghaisoict@gmail.com>
2022-05-10 16:33:34 +0700

    20220510

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,25 +1,20 @@
 import mysql from 'mysql2/promise';
 require('dotenv').config();
 
-var pool;
+console.log("Create connection pool at prod env ...");
+const pool = mysql.createPool({
+    host: "us-cdbr-east-05.cleardb.net",
+    database: "heroku_e3ab0ef9ae93fcb",
+    user: "be7c078ec85735",
+    password: "694e3149"
+})
 
-if (process.env.isTEST) {
-    console.log("Create connection pool at test env ...");
-    pool = mysql.createPool({
-        host: "localhost",
-        database: "CaroGame",
-        user: "haiht",
-        password: "123456"
-    })
-}
-else { // Production
-    console.log("Create connection pool at prod env ...");
-    pool = mysql.createPool({
-        host: "us-cdbr-east-05.cleardb.net",
-        database: "heroku_e3ab0ef9ae93fcb",
-        user: "be7c078ec85735",
-        password: "694e3149"
-    })
-}
+// console.log("Create connection pool at test env ...");
+// pool = mysql.createPool({
+//     host: "localhost",
+//     database: "CaroGame",
+//     user: "haiht",
+//     password: "123456"
+// })
 
 export default pool;
