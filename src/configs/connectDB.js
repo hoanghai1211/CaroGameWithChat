c423cbe418a6f496ccfae14cdf244eb9b46c6451
hoanghai1211 <hoanghaisoict@gmail.com>
2022-05-10 14:28:59 +0700

    20220510

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,13 +1,23 @@
 import mysql from 'mysql2/promise';
-// import pg from 'pg';
 
 console.log("Create connection pool...");
+var pool;
 
-const pool = mysql.createPool({
-    host: "localhost",
-    database: "CaroGame",
-    user: "haiht",
-    password: "123456"
-})
+if (isTEST) {
+    pool = mysql.createPool({
+        host: "localhost",
+        database: "CaroGame",
+        user: "haiht",
+        password: "123456"
+    })
+}
+else { // Production
+    pool = mysql.createPool({
+        host: "us-cdbr-east-05.cleardb.net",
+        database: "heroku_e3ab0ef9ae93fcb",
+        user: "be7c078ec85735",
+        password: "694e3149"
+    })
+}
 
 export default pool;
